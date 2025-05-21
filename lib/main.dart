13e03dbb9be92f28442193e8a1efc6612c59e537
alexandre pinto <alexandrepinto>
2025-05-20 21:49:19 -0300

    commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Center(
+    child: Text(
+      "utilizando flutter",
+      textDirection: TextDirection.ltr,
+      style: TextStyle(color: Colors.pink, fontSize: 30.0),
+    ),
+  ));
+}
+
